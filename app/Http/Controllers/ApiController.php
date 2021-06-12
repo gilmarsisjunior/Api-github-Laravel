a85de82604e727488408353cf384a86b07bd826e
Gilmar Silva Santos Junior <gilmar.sisjunior@gmail.com>
2021-06-11 22:05:23 -0300

    Atualizando

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,29 +9,32 @@ class ApiController extends Controller
     public function makeTheCallOfApi(Request $request)
     {     
         $username = $request->username;
-        
+
         $url = 'https://api.github.com/users/'.$username;
+
         // Inicia o curl
         $curl = curl_init();
 
         // Configura os parâmetros do curl
         curl_setopt_array($curl, [
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => $url,
-        CURLOPT_USERAGENT=> 1
+        CURLOPT_RETURNTRANSFER => 1, //O return será uma string e não um resultado direto
+        CURLOPT_URL => $url,         //A url para fetch
+        CURLOPT_USERAGENT=> 1        //User agent que possibilita a utilização da API
         ]);
+
         // Envio e armazenamento da resposta
         $response = json_decode(curl_exec($curl));
 
         $convertedToArray = get_object_vars($response);
-        if(count($convertedToArray) <10 ){
+
+        if(count($convertedToArray) < 10 ){
+            
             echo "Usuário incorreto, tente novamente em 3...";
             header( "refresh:3;url=http://127.0.0.1:8000" );
             return ;
 
         }
 
-        //Testa e trata um possível erro
         $login = $response->login;
         $id = $response->id;
         $email = $response->email ?? 'Este email não está disponível';
